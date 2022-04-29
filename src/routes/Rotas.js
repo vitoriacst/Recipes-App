0e08140936774dd068f8e4888eb4d005ddbbbcfe
Helder Medeiros <hmedeiros211@gmail.com>
2022-04-29 18:34:09 -0300

    tela de detalhes da receita

--- src/routes/Rotas.js
+++ src/routes/Rotas.js
@@ -10,15 +10,16 @@ import FavoritesRecipes from '../pages/FavoritesRecipes';
 import ScreenExplore from '../pages/ScreenExplore';
 import ExploreFoods from '../pages/ExploreFoods';
 import ExploreDrinks from '../pages/ExploreDrinks';
+import RecipeDetails from '../pages/RecipeDetails';
 
 function Rotas() {
   return (
     <Switch>
       <Route path="/profile" component={ Profile } />
-      <Route path="/foods" component={ Foods } />
-      <Route path="/foods/:idMeal" component={ Foods } />
-      <Route path="/drinks" component={ Drinks } />
-      <Route path="/drinks/:idDrink" component={ Drinks } />
+      <Route exact path="/foods" component={ Foods } />
+      <Route exact path="/foods/:idMeal" component={ RecipeDetails } />
+      <Route exact path="/drinks" component={ Drinks } />
+      <Route path="/drinks/:idDrink" component={ RecipeDetails } />
       <Route path="/teste" component={ BarraDeBusca } />
       <Route exact path="/" component={ Login } />
       <Route exact path="/done-recipes" component={ DoneRecipes } />
